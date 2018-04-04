03e368824932c9cb7613513c0405d778d3788cee
slillington <stephenlillington2017@u.northwestern.edu>
2018-04-04 15:00:06 -0500

    modularizing script

--- jsonMetaboliteExtraction.py
+++ jsonMetaboliteExtraction.py
@@ -1,27 +1,44 @@
 #Testing out metabolite name extraction from xml and json files
+'''
+Things this script needs to accomplish
+1. Isolate metabolite names from BiGG genome-scale model .json file
+2.
 
+'''
 import json
 
-input_file = open('Downloads\iAPECO1_1312.json','r')
-output_file = open('test.txt','w')
-#print(fil)
+def pullMetabolites():
+'''This function outputs a list of the names of metabolites in a BiGG database genome-scale model (.json format)
+'''
+   input_file = open('Downloads\iAPECO1_1312.json','r')
+   output_file = open('test.txt','w')
+    #print(fil)
    
-model = json.load(input_file)
+   model = json.load(input_file)
 
-metabolite_list = model.get('metabolites') #this is a list of json data
+   metabolite_list = model.get('metabolites') #this is a list of json data
 
-metabolites = []
-for item in metabolite_list:
-   met = item.get('name')
-   metabolites.append(met) 
+   metabolites = []
+   for item in metabolite_list:
+      met = item.get('name')
+      metabolites.append(met) 
      
-metabolites = list(set(metabolites)) #set removes duplicate values
+   metabolites = list(set(metabolites)) #set removes duplicate values
+   #output_file.write(str(metabolites)) #produces a list of names that needs to be cross-referenced with NIST
+   #Consider other identifiers if possible. Other option is formula. No more detail available
+   return metabolites
    
 
-output_file.write(str(metabolites)) #produces a list of names that needs to be cross-referenced with NIST
+   
+   
+   
+def main():
 
-#Consider other identifiers if possible. Other option is formula. No more detail available
 
+
+
+if __name__ == "__main__":
+    main()
    
   
 	
