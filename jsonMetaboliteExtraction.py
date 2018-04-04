a456dc2164f958a408de1a2d0fcb9d0d0ffedb0e
slillington <stephenlillington2017@u.northwestern.edu>
2018-04-04 16:14:07 -0500

    splitting script into functions
    Outputs metabolite list. .json file needed to be moved to same path.
    Executed from command line

--- jsonMetaboliteExtraction.py
+++ jsonMetaboliteExtraction.py
@@ -5,40 +5,45 @@ Things this script needs to accomplish
 2.
 
 '''
-import json
 
+
+import json
+	
 def pullMetabolites():
-'''This function outputs a list of the names of metabolites in a BiGG database genome-scale model (.json format)
-'''
-   input_file = open('Downloads\iAPECO1_1312.json','r')
-   output_file = open('test.txt','w')
+    #This function outputs a list of the names of metabolites in a BiGG database genome-scale model (.json format)
+    input_file = open('C:\GitHub\pythonScripts\MSlibrary-generation\iAPECO1_1312.json','r')
+    output_file = open('test.txt','w')
     #print(fil)
    
-   model = json.load(input_file)
+    model = json.load(input_file)
 
-   metabolite_list = model.get('metabolites') #this is a list of json data
+    metabolite_list = model.get('metabolites') #this is a list of json data
 
-   metabolites = []
-   for item in metabolite_list:
-      met = item.get('name')
-      metabolites.append(met) 
+    metabolites = []
+    for item in metabolite_list:
+        met = item.get('name')
+        metabolites.append(met) 
      
-   metabolites = list(set(metabolites)) #set removes duplicate values
-   #output_file.write(str(metabolites)) #produces a list of names that needs to be cross-referenced with NIST
-   #Consider other identifiers if possible. Other option is formula. No more detail available
-   return metabolites
+    metabolites = list(set(metabolites)) #set removes duplicate values
+    #output_file.write(str(metabolites)) #produces a list of names that needs to be cross-referenced with NIST
+    #Consider other identifiers if possible. Other option is formula. No more detail available
+    print(metabolites)
+    return metabolites
    
 
-   
-   
-   
 def main():
-
+    temp = pullMetabolites()
+    print(temp)
+    print('HELLO')
 
 
 
 if __name__ == "__main__":
     main()
+   
+   
+   
+
    
   
 	
